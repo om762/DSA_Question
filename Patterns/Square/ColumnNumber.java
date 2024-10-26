40508f0084dbb4b9912f81207c835215c34e452d
om762 <omprakash824rawat@gmail.com>
2024-10-26 18:52:50 +0530

    Edit

--- Patterns/Square/ColumnNumber.java
+++ Patterns/Square/ColumnNumber.java
@@ -1,8 +1,8 @@
 class ColumnNumber {
     public static void main(String[] args) {
         int n = 5;
-        for (int i = 0; i < n; i++){
-            for (int j = 0; j < n; j++) {
+        for (int i = 1; i <= n; i++){
+            for (int j = 1; j <= n; j++) {
                 System.out.print(j+ " ");
             }
             System.out.println();
--- Patterns/Square/RowNumber.java
+++ Patterns/Square/RowNumber.java
@@ -1,8 +1,8 @@
 class RowNumber {
     public static void main(String[] args) {
         int n = 5;
-        for (int i = 0; i < n; i++){
-            for (int j = 0; j < n; j++) {
+        for (int i = 1; i <= n; i++){
+            for (int j = 1; j <= n; j++) {
                 System.out.print(i + " ");
             }
             System.out.println();
